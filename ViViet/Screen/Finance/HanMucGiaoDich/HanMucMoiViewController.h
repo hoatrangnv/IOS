cb23eaceb73c917daaa99110a595a544ae89d63d
Tâm Nguyễn <nguyenvantam110@gmail.com>
2018-11-15 18:28:23 +0700

    Tamnv Thay Doi
    Dang lam han muc

--- ViViet/Screen/Finance/HanMucGiaoDich/HanMucMoiViewController.h
+++ ViViet/Screen/Finance/HanMucGiaoDich/HanMucMoiViewController.h
@@ -13,5 +13,20 @@
 
 @property (retain, nonatomic) IBOutlet TPKeyboardAvoidAcessory *scrMain;
 @property (retain, nonatomic) IBOutlet UIView *viewUI;
+@property (retain, nonatomic) IBOutlet NSLayoutConstraint *heightViewXacThuc;
+@property (retain, nonatomic) IBOutlet NSLayoutConstraint *heightViewMaXacThuc;
+@property (retain, nonatomic) IBOutlet UIButton *btnSMS;
+@property (retain, nonatomic) IBOutlet UIButton *btnToken;
+@property (retain, nonatomic) IBOutlet UIButton *btnMKPI;
+@property (retain, nonatomic) IBOutlet UIView *viewMaXacThuc;
+@property (retain, nonatomic) IBOutlet ExTextField *tfMaXacThuc;
+
+
+
+- (IBAction)suKienChonSMS:(id)sender;
+- (IBAction)suKienChonToken:(id)sender;
+- (IBAction)suKienChonMKPI:(id)sender;
+- (IBAction)suKienChonThucHien:(id)sender;
+
 
 @end
--- ViViet/Screen/Finance/HanMucGiaoDich/HanMucMoiViewController.m
+++ ViViet/Screen/Finance/HanMucGiaoDich/HanMucMoiViewController.m
@@ -15,22 +15,74 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view from its nib.
+    self.navigationItem.title = @"Hạn mức";
 }
 
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+- (void)viewWillAppear:(BOOL)animated {
+    [super viewWillAppear:animated];
 }
-*/
+
+- (void)viewDidAppear:(BOOL)animated {
+    [super viewDidAppear:animated];
+    if (![self.scrMain.subviews containsObject:self.viewUI]) {
+        self.viewUI.frame = CGRectMake(8, 8, self.scrMain.frame.size.width - 16, self.viewUI.frame.size.height - self.heightViewMaXacThuc.constant);
+        self.viewUI.layer.cornerRadius = 8.0;
+        [self.scrMain addSubview:self.viewUI];
+        self.heightViewXacThuc.constant -= self.heightViewMaXacThuc.constant;
+        self.heightViewMaXacThuc.constant = 0.0;
+        [self.viewMaXacThuc setHidden:YES];
+        [self.scrMain setContentSize:CGSizeMake([UIScreen mainScreen].bounds.size.width, self.viewUI.frame.size.height)];
+    }
+}
+
+- (IBAction)suKienChonSMS:(id)sender {
+    if (self.heightViewMaXacThuc.constant > 0) {
+        CGRect frame = self.viewUI.frame;
+        self.heightViewXacThuc.constant -= self.heightViewMaXacThuc.constant;
+        frame.size.height -= self.heightViewMaXacThuc.constant;
+        self.viewUI.frame = frame;
+        self.heightViewMaXacThuc.constant = 0;
+    }
+    [self.viewMaXacThuc setHidden:YES];
+}
+
+- (IBAction)suKienChonToken:(id)sender {
+    if (self.heightViewMaXacThuc.constant == 0.0) {
+        self.heightViewMaXacThuc.constant = 40.0;
+        self.heightViewXacThuc.constant += self.heightViewMaXacThuc.constant;
+        CGRect frame = self.viewUI.frame;
+        frame.size.height += self.heightViewMaXacThuc.constant;
+        self.viewUI.frame = frame;
+    }
+    [self.tfMaXacThuc setPlaceholder:@"Mật khẩu token"];
+    [self.viewMaXacThuc setHidden:NO];
+}
+
+- (IBAction)suKienChonMKPI:(id)sender {
+    if (self.heightViewMaXacThuc.constant == 0.0) {
+        self.heightViewMaXacThuc.constant = 40.0;
+        self.heightViewXacThuc.constant += self.heightViewMaXacThuc.constant;
+        CGRect frame = self.viewUI.frame;
+        frame.size.height += self.heightViewMaXacThuc.constant;
+        self.viewUI.frame = frame;
+    }
+    [self.tfMaXacThuc setPlaceholder:@"Chữ ký mPKI"];
+    [self.viewMaXacThuc setHidden:NO];
+}
+
+- (IBAction)suKienChonThucHien:(id)sender {
+}
 
 - (void)dealloc {
     [_scrMain release];
     [_viewUI release];
+    [_heightViewXacThuc release];
+    [_heightViewMaXacThuc release];
+    [_btnSMS release];
+    [_btnToken release];
+    [_btnMKPI release];
+    [_viewMaXacThuc release];
+    [_tfMaXacThuc release];
     [super dealloc];
 }
 @end
